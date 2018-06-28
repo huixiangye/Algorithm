1d761cd9f78a5933ce368bac8b386d7176300938
huixiangye <hxy153530@gmail.com>
2018-06-28 17:06:56 -0500

    fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,44 @@
+/**
+ * Created by yehuixiang on 5/14/18.
+ */
+public class Fibonacci {
+
+    //recursion
+    public int fibo(int n){
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        //System.out.print(" " +fibo(n-1) + fibo(n-2) );
+        return fibo(n-1) + fibo(n-2);
+    }
+
+    //without using any collection
+    public int fibo2(int n){
+        int a = 0;
+        int b = 1;
+        int c;
+
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        for(int i = 2;i<=n;i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+
+    }
+
+
+    public static void main(String[] args){
+        Fibonacci test1 = new Fibonacci();
+        System.out.println(test1.fibo(9));
+        System.out.println(test1.fibo2(9));
+    }
+}
