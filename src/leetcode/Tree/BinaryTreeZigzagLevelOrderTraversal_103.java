527683902bd359324b5331533d256412f257950e
ryan <hxy153530@gmail.com>
2019-11-18 22:26:05 +0800

    leetcode

--- src/leetcode/Tree/BinaryTreeZigzagLevelOrderTraversal_103.java
+++ src/leetcode/Tree/BinaryTreeZigzagLevelOrderTraversal_103.java
@@ -4,22 +4,22 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-//Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
-//
-//        For example:
-//        Given binary tree [3,9,20,null,null,15,7],
-//        3
-//        / \
-//        9  20
-//        /  \
-//        15   7
-//        return its zigzag level order traversal as:
-//        [
-//        [3],
-//        [20,9],
-//        [15,7]
-//        ]
+/* Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
 
+        For example:
+        Given binary tree [3,9,20,null,null,15,7],
+        3
+        / \
+        9  20
+        /  \
+        15   7
+        return its zigzag level order traversal as:
+        [
+        [3],
+        [20,9],
+        [15,7]
+        ]
+*/
 public class BinaryTreeZigzagLevelOrderTraversal_103 {
     public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new LinkedList<>();
